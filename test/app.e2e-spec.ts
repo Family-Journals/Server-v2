bcf9a302b2429478c2efe1548c5fcef84fd59a29
sjordan20 <jordanstephen949@gmail.com>
2023-10-23 11:02:10 -0600

    add-tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,55 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+import { AppModule } from '../src/app.module';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import * as pactum from 'pactum';
+import { CreateUserDto } from 'src/user/dto';
 
-describe('AppController (e2e)', () => {
+describe('App e2e', () => {
   let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
     await app.init();
+    await app.listen(3333);
+    pactum.request.setBaseUrl('http://localhost:3333');
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(() => {
+    app.close();
+  });
+
+  describe('Auth', () => {
+    describe('Create User', () => {
+      const dto: CreateUserDto = {
+        email: 'testuser@test.com',
+        password: 'testPassword',
+        confirmPassword: 'testPassword',
+      };
+      it('Should create user', () => {
+        // return pactum.spec().post('/users').withBody(dto).expectStatus(201);
+      });
+
+      it('Should throw if email is empty', () => {
+        delete dto.email;
+        return pactum
+          .spec()
+          .post('/users')
+          .withBody(dto)
+          .expectStatus(400)
+          .inspect();
+      });
+
+      it('Should throw if password is empty', () => {
+        delete dto.password;
+        return pactum.spec().post('/users').withBody(dto).expectStatus(400);
+      });
+
+      it('Should throw if confirmPassword is empty', () => {
+        delete dto.confirmPassword;
+        return pactum.spec().post('/users').withBody(dto).expectStatus(400);
+      });
+    });
   });
 });
